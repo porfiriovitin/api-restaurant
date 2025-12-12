336e9151c26e74eed8953623af449b2f64f66ca5
porfiriovitin <victorhugo.svm@hotmail.com>
2025-12-11 22:49:34 -0300

    Feat: Implement product creation with validation and error handling

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,13 +1,32 @@
 import { NextFunction, Request, Response } from "express";
+import { knex } from "@/database/knex";
+
+import {z} from "zod"
 
 class ProductController {
     async index (request: Request, response: Response, next:NextFunction){
         try{
-            
-
             return response.json({message: "Ok"})
-
         }catch(error){
+            next(error)
+        }
+    }
+
+    async create(request:Request, response:Response, next:NextFunction){
+        try{
+
+            const bodySchema = z.object({
+                name: z.string().trim().min(4).nonempty({message: "Name is required"}),
+                price: z.number().gt(0, {message: "Price must be greater than zero."})
+            })
+
+            const {name, price} = bodySchema.parse(request.body)
+
+            await knex<ProductRepository>("products").insert({ name: name, price: price })
+
+            return response.status(201).json()
+
+        } catch(error){
             next(error)
         }
     }
